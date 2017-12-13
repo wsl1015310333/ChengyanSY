5dfc9880ade294023f8883366fa97cc0e7eaf934
wsl <1015310333@qq.com>
2017-12-13 13:37:55 +0800

    一车一档

--- app/src/main/java/searchproject/php/yisa/chengyansy/Model_All/ACarACrime.java
+++ app/src/main/java/searchproject/php/yisa/chengyansy/Model_All/ACarACrime.java
@@ -18,6 +18,264 @@ public class ACarACrime implements Serializable {
     private String type;
     private String numCard;
 
+    private String cllx;
+    private String clxh;
+    private String  hmzl;
+
+
+
+
+    private String clyt;//c
+    private String clsbdh;
+    private String djzsbh;
+    private String dabh;
+    private String syxz;
+    private String hdfs;
+   private String  syqxz;
+   private String  jdczt;
+   private String  dybj;
+   private String  zjdjrq;
+   private String  ycyxqz;
+   private String  qzbfqz;
+   private String  zsxxdz;
+   private String  zzxxdz;
+   private String  zzjzzm;
+   private String  ccdjrq;
+   private String  bxzzrq;
+   private String  gxsj;
+   private String  fprq;
+   private String  fhjzrq;
+   private String  fdjzsrq;
+   private String  bhlhszcs;
+   private String  bhlhpcs;
+   private String  bhlzscs;
+   private String   glbm;
+
+
+    public String getClyt() {
+        return clyt;
+    }
+
+    public void setClyt(String clyt) {
+        this.clyt = clyt;
+    }
+
+    public String getClsbdh() {
+        return clsbdh;
+    }
+
+    public void setClsbdh(String clsbdh) {
+        this.clsbdh = clsbdh;
+    }
+
+    public String getDjzsbh() {
+        return djzsbh;
+    }
+
+    public void setDjzsbh(String djzsbh) {
+        this.djzsbh = djzsbh;
+    }
+
+    public String getDabh() {
+        return dabh;
+    }
+
+    public void setDabh(String dabh) {
+        this.dabh = dabh;
+    }
+
+    public String getSyxz() {
+        return syxz;
+    }
+
+    public void setSyxz(String syxz) {
+        this.syxz = syxz;
+    }
+
+    public String getHdfs() {
+        return hdfs;
+    }
+
+    public void setHdfs(String hdfs) {
+        this.hdfs = hdfs;
+    }
+
+    public String getSyqxz() {
+        return syqxz;
+    }
+
+    public void setSyqxz(String syqxz) {
+        this.syqxz = syqxz;
+    }
+
+    public String getJdczt() {
+        return jdczt;
+    }
+
+    public void setJdczt(String jdczt) {
+        this.jdczt = jdczt;
+    }
+
+    public String getDybj() {
+        return dybj;
+    }
+
+    public void setDybj(String dybj) {
+        this.dybj = dybj;
+    }
+
+    public String getZjdjrq() {
+        return zjdjrq;
+    }
+
+    public void setZjdjrq(String zjdjrq) {
+        this.zjdjrq = zjdjrq;
+    }
+
+    public String getYcyxqz() {
+        return ycyxqz;
+    }
+
+    public void setYcyxqz(String ycyxqz) {
+        this.ycyxqz = ycyxqz;
+    }
+
+    public String getQzbfqz() {
+        return qzbfqz;
+    }
+
+    public void setQzbfqz(String qzbfqz) {
+        this.qzbfqz = qzbfqz;
+    }
+
+    public String getZsxxdz() {
+        return zsxxdz;
+    }
+
+    public void setZsxxdz(String zsxxdz) {
+        this.zsxxdz = zsxxdz;
+    }
+
+    public String getZzxxdz() {
+        return zzxxdz;
+    }
+
+    public void setZzxxdz(String zzxxdz) {
+        this.zzxxdz = zzxxdz;
+    }
+
+    public String getZzjzzm() {
+        return zzjzzm;
+    }
+
+    public void setZzjzzm(String zzjzzm) {
+        this.zzjzzm = zzjzzm;
+    }
+
+    public String getCcdjrq() {
+        return ccdjrq;
+    }
+
+    public void setCcdjrq(String ccdjrq) {
+        this.ccdjrq = ccdjrq;
+    }
+
+    public String getBxzzrq() {
+        return bxzzrq;
+    }
+
+    public void setBxzzrq(String bxzzrq) {
+        this.bxzzrq = bxzzrq;
+    }
+
+    public String getGxsj() {
+        return gxsj;
+    }
+
+    public void setGxsj(String gxsj) {
+        this.gxsj = gxsj;
+    }
+
+    public String getFprq() {
+        return fprq;
+    }
+
+    public void setFprq(String fprq) {
+        this.fprq = fprq;
+    }
+
+    public String getFhjzrq() {
+        return fhjzrq;
+    }
+
+    public void setFhjzrq(String fhjzrq) {
+        this.fhjzrq = fhjzrq;
+    }
+
+    public String getFdjzsrq() {
+        return fdjzsrq;
+    }
+
+    public void setFdjzsrq(String fdjzsrq) {
+        this.fdjzsrq = fdjzsrq;
+    }
+
+    public String getBhlhszcs() {
+        return bhlhszcs;
+    }
+
+    public void setBhlhszcs(String bhlhszcs) {
+        this.bhlhszcs = bhlhszcs;
+    }
+
+    public String getBhlhpcs() {
+        return bhlhpcs;
+    }
+
+    public void setBhlhpcs(String bhlhpcs) {
+        this.bhlhpcs = bhlhpcs;
+    }
+
+    public String getBhlzscs() {
+        return bhlzscs;
+    }
+
+    public void setBhlzscs(String bhlzscs) {
+        this.bhlzscs = bhlzscs;
+    }
+
+    public String getGlbm() {
+        return glbm;
+    }
+
+    public void setGlbm(String glbm) {
+        this.glbm = glbm;
+    }
+
+    public String getCllx() {
+        return cllx;
+    }
+
+    public void setCllx(String cllx) {
+        this.cllx = cllx;
+    }
+
+    public String getClxh() {
+        return clxh;
+    }
+
+    public void setClxh(String clxh) {
+        this.clxh = clxh;
+    }
+
+    public String getHmzl() {
+        return hmzl;
+    }
+
+    public void setHmzl(String hmzl) {
+        this.hmzl = hmzl;
+    }
+
     public String getNumCard() {
         return numCard;
     }
